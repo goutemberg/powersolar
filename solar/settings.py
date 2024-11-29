1c0d31e5e4c429bd75c9f14d3aa4ac4ddfe80735
goutembergpessoa <goutemberg.silva@foxbit.com.br>
2024-11-29 10:41:56 -0300

    mais alteracoes de ajutes

--- solar/settings.py
+++ solar/settings.py
@@ -161,7 +161,7 @@ if not DEBUG:
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, './home/static/solar'),
+    os.path.join(BASE_DIR, 'home/static'),
 ]
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
