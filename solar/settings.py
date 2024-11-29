8d4942626f737ad16fd6ffd62a83a888b71ead03
goutembergpessoa <goutemberg.silva@foxbit.com.br>
2024-11-29 11:02:58 -0300

    testes

--- solar/settings.py
+++ solar/settings.py
@@ -154,7 +154,7 @@ MEDIA_URL = '/media/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-MEDIA_ROOT = os.path.join(BASE_DIR, '/data/web/media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 if not DEBUG:
     
